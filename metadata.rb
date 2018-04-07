ab17e7045d7b1427481e0b2c867b697bfc083d34
Michael Burns <michael@mirwin.net>
2018-04-07 12:23:25 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 maintainer 'Michael Burns'
 maintainer_email 'michael@mirwin.net'
 license 'BSD-2-Clause'
-version '0.2.1'
+version '0.2.2'
 
 supports 'centos'
 supports 'ubuntu'
